61cba9227f8e3b2a7727f575faa0a9aabd7cd7d2
soeb003 <bsoe@ucsd.edu>
2014-02-09 18:46:09 -0800

    Skeleton Webpage

--- javascript/script.js
+++ javascript/script.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
-	$(window).scroll(function(){
-		if($(this).scrollTop() > 100)
-		{
-			$('.scrollup').fadeIn();
-		}
-		else
-		{
-			$('.scrollup').fadeOut();
-		}
-	});
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 100)
+    {
+      $('.scrollup').fadeIn();
+    }
+    else
+    {
+      $('.scrollup').fadeOut();
+    }
+  });
 });
 
 $("#nav > li > div").click(function(){
-	if(false == $(this).next().is(':visible'))
-	{
-		$('#nav ul').slideUp(350);
-	}
-	$(this).next().slideToggle(300);
+  if(false == $(this).next().is(':visible'))
+  {
+    $('#nav ul').slideUp(350);
+  }
+  $(this).next().slideToggle(300);
 });
 $('#nav ul:eq(0)').show();
